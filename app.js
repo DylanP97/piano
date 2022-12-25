cab67f2db9e38196fec5f745b7322fa3666dcc48
DylanP97 <dylan.pinheiro.1997@gmail.com>
2022-12-25 22:01:57 +0100

    style, multiple touch & touchend

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const pianoKeys = document.querySelectorAll('.key')
+const pianoKeys = document.querySelectorAll(".key");
+
+// Log events flag
+const logEvents = false;
+
+// Touch Point cache
+const tpCache = [];
 
 function playSound(newUrl) {
-    console.log(newUrl)
-    new Audio(newUrl).play()
+  new Audio(newUrl).play();
 }
 
-pianoKeys.forEach((pianoKey, i)  => {
-    const number = i < 9 ? '0' + (i + 1) : (i + 1 );
-    const newUrl = '24-piano-keys/key-' + number + '.mp3'
-    pianoKey.addEventListener('click', () => playSound(newUrl))
-})
+pianoKeys.forEach((pianoKey, i) => {
+  const number = i < 9 ? "0" + (i + 1) : i + 1;
+  const newUrl = "24-piano-keys/key-" + number + ".mp3";
+  pianoKey.addEventListener("touchstart", function (e) {
+    e.preventDefault();
+    e.target.classList.add("keyOrange");
+    playSound(newUrl);
+  });
+  pianoKey.addEventListener("touchend", function (e) {
+    e.preventDefault();
+    e.target.classList.remove("keyOrange");
+  });
+});
